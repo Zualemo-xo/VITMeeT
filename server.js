26c514e29747f172bf10899e88ea09fe3d315658
Zualemo-xo <maheysh.v2019@vitstudent.ac.in>
2021-04-12 19:32:48 +0530

    added from local

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
+
+app.listen(3000, function() {
+  console.log("Running on port 3000.");
+});
